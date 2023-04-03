6c39c15937cc4f994922379388dfd9a4ed142d26
kalom60 <kaleabtesfaye00@gmail.com>
2023-04-03 16:26:47 +0300

    get router added

--- server/router/index.ts
+++ server/router/index.ts
@@ -4,5 +4,6 @@ import FileController from "../controller/FIleController";
 const router: express.Router = express.Router();
 
 router.post("/file", FileController.addFile);
+router.get("/file", FileController.getFiles);
 
 export default router;
