df21181753785aeef57297f70fc3c0d8ab740ab1
kalom60 <kaleabtesfaye00@gmail.com>
2023-04-03 16:55:10 +0300

    antd table and button added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Button, Table, Modal } from "antd";
+import type { ColumnsType } from "antd/es/table";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+interface DataType {
+  id: string;
+  fileName: string;
+  key: React.Key;
 }
 
-export default App
+const App: React.FC = () => {
+  const [files, setFiles] = useState<any>([]);
+  const [attach, setAttach] = useState<boolean>(false);
+
+  const fetchData = async () => {
+    await axios({
+      method: "GET",
+      url: "http://localhost:5000/file",
+    })
+      .then((res) => {
+        res.data.map((file: any) => {
+          file.key = file.id;
+        });
+        setFiles(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: "File Name",
+      dataIndex: "fileName",
+    },
+    {
+      title: "File Size",
+      dataIndex: "fileSize",
+    },
+    {
+      title: "Uploaded Date",
+      dataIndex: "createdAt",
+    },
+  ];
+
+  return (
+    <div>
+      <div>
+        <Button type="primary" onClick={() => setAttach(true)}>
+          upload
+        </Button>
+      </div>
+
+      <Table columns={columns} dataSource={files} size="middle" />
+    </div>
+  );
+};
+
+export default App;
