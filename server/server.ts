a4cfd7935902269e7721299eac8eba95d1775f14
kalom60 <kaleabtesfaye00@gmail.com>
2023-04-03 16:10:03 +0300

    intialize router

--- server/server.ts
+++ server/server.ts
@@ -3,13 +3,21 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import fileUpload from "express-fileupload";
 
+import router from "./router";
+import db from "./model";
+
 const PORT: number = 5000;
 const app = express();
+
+(async () => {
+  await db.sequelize.sync();
+})();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(fileUpload());
 app.use(cors());
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
