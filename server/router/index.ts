f5966154f50f34906369250d0feca8f81cda2a17
kalom60 <kaleabtesfaye00@gmail.com>
2023-04-03 16:23:49 +0300

    post router added

--- server/router/index.ts
+++ server/router/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import FileController from "../controller/FIleController";
 
 const router: express.Router = express.Router();
+
+router.post("/file", FileController.addFile);
 
 export default router;
